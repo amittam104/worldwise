070b06aa4f39abc05b36959940a395372a927f81
Amit Tambulkar <amittambulkar104@gmail.com>
2024-07-06 12:25:28 +0530

    chore: add nested routes, update app nav

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="/product" element={<Product />}></Route>
-        <Route path="/pricing" element={<Pricing />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/app" element={<AppLayout />}></Route>
-        <Route path="*" element={<Error404 />}></Route>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
@@ -9,7 +10,8 @@ function Sidebar() {
       <Logo />
       <AppNav />
 
-      <p>List of Cities</p>
+      <Outlet />
+
       <Footer />
     </div>
   );
